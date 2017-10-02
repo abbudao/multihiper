3be205a3c9d8e955dd301311153d3b50e8f1b054
Abbud-Desktop <abbudao@gmail.com>
2017-10-02 19:18:42 -0300

    Starting YCbCr functions \ Refactorated some codes

--- projeto1/bmp.h
+++ projeto1/bmp.h
@@ -33,7 +33,7 @@ typedef struct {
     unsigned int   biClrImportant;  /* Number of important colors */
 } BMPINFOHEADER;
 
-void RGB2YCbCr(unsigned char **R, unsigned char **G, unsigned char **B, int Heigth, int Width, double **Y, double **Cb, double **Cr, FILE *entrada);
+unsigned char *** RGB2YCbCr(unsigned char ***rgb_channel, BMPINFOHEADER header);
 void YCbCr2RGB (double **Y, double **Cb, double **Cr, int Height, int Width, double **R, double **G, double **B);
 void writeR(unsigned char **R, BMPMAGICNUMBER bmpnum, BMPFILEHEADER fHeader, BMPINFOHEADER header);
 void writeG(unsigned char **G, BMPMAGICNUMBER bmpnum, BMPFILEHEADER fHeader, BMPINFOHEADER header);
@@ -41,10 +41,11 @@ void writeB(unsigned char **B, BMPMAGICNUMBER bmpnum, BMPFILEHEADER fHeader, BMP
 void writeY(double **Y, BMPMAGICNUMBER bmpnum, BMPFILEHEADER fHeader, BMPINFOHEADER header);
 void debug_bmp(BMPMAGICNUMBER bmpnum,BMPFILEHEADER fHeader,BMPINFOHEADER header);
 void debug_channels(unsigned char *** channels, BMPINFOHEADER header);
-
+unsigned char *** intialize_channels(BMPINFOHEADER header);
+void header_write(FILE *fSaida,BMPMAGICNUMBER bmpnum,BMPFILEHEADER fHeader,BMPINFOHEADER header);
 void debug_bmp(BMPMAGICNUMBER bmpnum,BMPFILEHEADER fHeader,BMPINFOHEADER header)
 {
-    printf("\n bmpnum: %d", bmpnum);
+    printf("\n bmpnum: %d", bmpnum.bfType);
     printf("\n offBits: %d", fHeader.bfOffBits);
     printf("\n Reserved1: %d", fHeader.bfReserved1);
     printf("\n Reserved2: %d", fHeader.bfReserved2);
@@ -72,4 +73,46 @@ void debug_channels(unsigned char *** channels, BMPINFOHEADER header){
     }
   }
 }
+unsigned char ***intialize_channels(BMPINFOHEADER header) {
+  int i, j;
+  unsigned char ***channels;
+  channels = malloc(3 * sizeof(unsigned char **));
+  if (channels == NULL) {
+    printf("Memory exceeded");
+  }
+  for (i = 0; i < 3; i++) {
+    channels[i] = malloc(sizeof(unsigned char *) * header.biHeight);
+    if (channels[i] == NULL) {
+      printf("Memory exceeded");
+    }
+  }
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < header.biHeight; j++) {
+      channels[i][j] = malloc(sizeof(unsigned char) * header.biWidth);
+      if (channels[i][j] == NULL) {
+        printf("Memory exceeded");
+      }
+    }
+  }
+  return channels;
+}
+void header_write(FILE *fSaida,BMPMAGICNUMBER bmpnum,BMPFILEHEADER fHeader,BMPINFOHEADER header) {
+  fwrite(&bmpnum, 2, 1, fSaida);
+  fwrite(&fHeader, sizeof(BMPFILEHEADER), 1, fSaida);
+  fwrite(&header, sizeof(BMPINFOHEADER), 1, fSaida);
+}
+
+unsigned char *** RGB2YCbCr(unsigned char ***rgb_channels, BMPINFOHEADER header){
+unsigned char *** YCbCr_channels= intialize_channels(header);
+  for (int i = 0; i < header.biHeight; i++) {
+    for (int j = 0; j < header.biWidth; j++) {
+      /* printf("%d %d %d", b, g, r); */
+      YCbCr_channels[0][i][j] = (unsigned char) (0.114*(float)rgb_channels[0][i][j]+0.587*(float)rgb_channels[1][i][j]+0.299*(float)rgb_channels[2][i][j]);
+       YCbCr_channels[1][i][j] = (unsigned char) 0.564*((float)rgb_channels[0][i][j]-(float)YCbCr_channels[0][i][j]);
+       YCbCr_channels[1][i][j] = (unsigned char) 0.713*((float)rgb_channels[2][i][j]-(float)YCbCr_channels[0][i][j]);
+    }}
+return YCbCr_channels;
+}
+
+
 #endif
--- projeto1/compressor.c
+++ projeto1/compressor.c
@@ -8,37 +8,41 @@ FILE *fSaida;
 BMPMAGICNUMBER bmpnum;
 BMPFILEHEADER fHeader;
 BMPINFOHEADER header;
-unsigned char ***channels;
 
 int read_io(char *a, char *b);
 int error_happened(FILE *file, char *name);
 void close_io();
 void bmp_magic();
-int header_write();
-void intialize_channels(BMPINFOHEADER header, unsigned char ***channels);
+unsigned char *** fileto_rgb(BMPINFOHEADER header,FILE *fEntrada,unsigned char *** channels);
 void read_channels(FILE *filein, unsigned char ***channels);
-/* void free_channels(unsigned char *** channels); */
+void free_channels(BMPINFOHEADER header,unsigned char *** channels); 
 unsigned char bitsToByte(int bits[]);
+void rgb_tofile(unsigned char ***channels,FILE *fSaida);
 
 int main(int argc, char *argv[]) {
   /*Read the input file and output file */
+  unsigned char ***channels;
   printf("intializing \n");
   printf("Read_io \n");
   read_io(argv[1], argv[2]);
   printf("bmp_magic \n");
   bmp_magic();
   printf("debug_bmp \n");
-  debug_bmp(bmpnum, fHeader, header);
+  /* debug_bmp(bmpnum, fHeader, header); */
   printf("intialize_channels \n");
-  intialize_channels(header, channels);
+  channels=  intialize_channels(header);
+  channels= fileto_rgb(header,fEntrada, channels);
+  printf("%d",channels[0][1][2]);
   printf("read_channels \n");
   /* read_channels(fEntrada, channels); */
   printf("debug_channels \n");
   /*  debug_channels(channels, header);*/
   printf("header_Write\n");
-  header_write(fSaida);
+  header_write(fSaida,bmpnum,fHeader,header);
+  printf("rgb_tofile\n");
+  rgb_tofile(channels,fSaida);
   printf("free \n");
-  free(channels);
+  free_channels(header,channels);
   printf("close_io \n");
   close_io();
   return 0;
@@ -60,20 +64,20 @@ int error_happened(FILE *file, char *name) {
   if (file == NULL) {
     printf("File named '%s' open error: ", name);
     switch (errno) {
-    case EPERM:
-      printf("Operation not permitted\n");
-      break;
-    case ENOENT:
-      printf("File not found\n");
-      break;
-    case EACCES:
-      printf("Permission denied\n");
-      break;
-    case ENAMETOOLONG:
-      printf("Filename is too long\n");
-      break;
-    default:
-      printf("Unknown error\n");
+      case EPERM:
+        printf("Operation not permitted\n");
+        break;
+      case ENOENT:
+        printf("File not found\n");
+        break;
+      case EACCES:
+        printf("Permission denied\n");
+        break;
+      case ENAMETOOLONG:
+        printf("Filename is too long\n");
+        break;
+      default:
+        printf("Unknown error\n");
     }
     return (1);
   } else {
@@ -92,32 +96,11 @@ void bmp_magic() {
   fread(&header, sizeof(BMPINFOHEADER), 1, fEntrada);
 }
 
-int header_write(FILE *fSaida) {
-  fwrite(&bmpnum, 2, 1, fSaida);
-  fwrite(&fHeader, sizeof(BMPFILEHEADER), 1, fSaida);
-  fwrite(&header, sizeof(BMPINFOHEADER), 1, fSaida);
-  return 1;
-}
-void intialize_channels(BMPINFOHEADER header, unsigned char ***channels) {
-  int i, j;
-  channels = malloc(3 * sizeof(unsigned char **));
-  if (channels == NULL) {
-    printf("Memory exceeded");
-  }
-  for (i = 0; i < 3; i++) {
-    channels[i] = malloc(sizeof(unsigned char *) * header.biHeight);
-    if (channels[i] == NULL) {
-      printf("Memory exceeded");
-    }
-  }
-  for (i = 0; i < 3; i++) {
-    for (j = 0; j < header.biHeight; j++) {
-      channels[i][j] = malloc(sizeof(unsigned char) * header.biWidth);
-      if (channels[i][j] == NULL) {
-        printf("Memory exceeded");
-      }
-    }
-  }
+
+
+
+unsigned char *** fileto_rgb(BMPINFOHEADER header,FILE *fEntrada,unsigned char *** channels) {
+  int i,j,b,g,r;
   fseek(fEntrada, 54, SEEK_SET);
   printf("Seek is fine");
   for (i = 0; i < header.biHeight; i++) {
@@ -125,17 +108,20 @@ void intialize_channels(BMPINFOHEADER header, unsigned char ***channels) {
       int b = fgetc(fEntrada);
       int g = fgetc(fEntrada);
       int r = fgetc(fEntrada);
-      printf("%d %d %d", b, g, r);
+      /* printf("%d %d %d", b, g, r); */
       channels[0][i][j] = (unsigned char)b;
       channels[1][i][j] = (unsigned char)g;
       channels[2][i][j] = (unsigned char)r;
     }
   }
+  return channels;
 }
 
-void free_channels(unsigned char ***channels) {
-  printf("Start of channels!");
+void free_channels(BMPINFOHEADER header,unsigned char ***channels) {
   for (int i = 0; i < 3; i++) {
+    for(int j=0;j<header.biHeight;j++){
+      free(channels[i][j]);
+    }
     free(channels[i]);
   }
 }
@@ -150,4 +136,13 @@ unsigned char bitsToByte(int bits[]) {
   meuByte += bits[i];
 
   return meuByte;
+}
+void rgb_tofile(unsigned char ***channels,FILE *fSaida){
+  for (int i = 0; i < header.biHeight; i++) {
+    for (int j = 0; j < header.biWidth; j++) {
+      fputc(channels[0][i][j],fSaida);
+      fputc(channels[1][i][j],fSaida);
+      fputc(channels[2][i][j],fSaida);
+    }
+  }
 }
